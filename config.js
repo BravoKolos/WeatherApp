d85b8a5a58cc6f30437c337da41e8ae17ab4f1a3
BravoKolos <kamilkolos4321@gmail.com>
2024-07-29 09:31:12 +0200

    adding updated files

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiKey: "f650c3891c4c2457a7513e6f90b6309d"
+};
+
+export default config;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "f650c3891c4c2457a7513e6f90b6309d";
 
+const apiKey = window.config.apiKey;
 
 weatherForm.addEventListener("submit", async event => {
     event.preventDefault();
