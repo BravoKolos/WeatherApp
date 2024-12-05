2f58ac5b96338ef5bfa2528a5564231b3172f5bd
BravoKolos <kamilkolos4321@gmail.com>
2024-12-05 09:32:31 +0100

    minor code update based on code review

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const weatherForm = document.querySelector(".weatherForm");
-const cityInput = document.querySelector(".cityInput");
-const card = document.querySelector(".card");
+const weatherForm = document.querySelector("#weatherForm");
+const cityInput = document.querySelector("#cityInput");
+const card = document.querySelector("#card");
 
 const apiKey = window.config.apiKey;
 
